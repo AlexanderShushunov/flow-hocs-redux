1cf2f825d5e791e604f200bf75b3ab559e53dfc4
Alexandr Shushunov <ashushunov@mail.ru>
2018-02-07 15:23:54 +0300

    Add more complicated hoc

---
+++ src/complicated-hoc/complicated-inject-hoc-detailed.js
@@ -0,0 +1,56 @@
+// @flow
+
+import * as React from 'react';
+
+// TODO How to avoid Injector
+type Injector<OwnProps: {}, InjectProps: {}> =
+    (WrappedComponent: React.ComponentType<OwnProps & InjectProps>) => React.ComponentType<$Exact<OwnProps>>;
+
+function injectMoreProp<OwnProps: {}, InjectProps: {}>(
+    findOutMoreProps: OwnProps => InjectProps
+): Injector<OwnProps, InjectProps> {
+    return WrappedComponent =>
+        function WrapperComponent(ownProps: OwnProps) {
+            return <WrappedComponent {...ownProps} {...findOutMoreProps(ownProps)} />;
+        };
+}
+
+// TODO Dose not work if the pros is an exact object.
+class MyComponent extends React.Component<{
+    shouldBePassed: number,
+    injected: number
+}> {
+    render() {
+        const {
+            shouldBePassed,
+            injected
+        } = this.props;
+        return shouldBePassed.toFixed() + injected.toFixed();
+    }
+}
+
+type OwnProps =  {
+    shouldBePassed: number
+};
+
+type InjectProps = {
+    injected: number
+};
+
+const findOutMoreProps: OwnProps => InjectProps = ({shouldBePassed}) => ({injected: shouldBePassed + 42});
+
+const injector: Injector<OwnProps, InjectProps> = injectMoreProp(findOutMoreProps);
+
+const MyEnhancedComponent = injector(MyComponent);
+
+export const Usage = () => (
+    <div>
+        <MyEnhancedComponent shouldBePassed={-1} />
+        {/*<MyEnhancedComponent shouldBePassed={'bla-bla'} /> runtime error*/}
+        {/*<MyEnhancedComponent /> runtime error*/}
+        {/*<MyEnhancedComponent shouldBePassed={-1} injected={3} /> erase prop*/}
+        {/*<MyEnhancedComponent shouldBePassed={-1} injected={'text'} /> erase prop with wrong type*/}
+        {/*<MyEnhancedComponent shouldBePassed={-1} otherProp={'text'} /> unnecessary prop*/}
+    </div>
+);
+
